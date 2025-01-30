f204573817ddf3e5c1e81fc026a30060bee8c0c9
ovkulkarni4 <74922976+ovkulkarni4@users.noreply.github.com>
2025-01-30 10:55:44 -0800

    Update metrics_utils.py

--- metrics_utils.py
+++ metrics_utils.py
@@ -1,21 +1,21 @@
 # For simple accuracy
-def accuracy_fn(y_true, y_pred):
+def simple_accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
     acc = (correct / len(y_pred)) * 100
     return acc
 
 # For multiclass accuracy
-def accuracy_fn(y_true, logits):
+def multiclass_accuracy_fn(y_true, logits):
     y_pred = torch.argmax(logits, dim=1)
     return (y_true == y_pred).float().mean().item() * 100
 
 # For pipeline with softmax
-def accuracy_fn(y_true, logits):
+def pipeline_with_softmax_accuracy_fn(y_true, logits):
     y_pred = torch.softmax(logits, dim=1).argmax(dim=1)
     return (y_true == y_pred).float().mean().item() * 100
 
 # device handling and dtype checking
-def accuracy_fn(y_true, y_pred):
+def device_and_dtype_accuracy_fn(y_true, y_pred):
     # Move to same device and type
     y_true = y_true.to(y_pred.device)
     y_true = y_true.long()
